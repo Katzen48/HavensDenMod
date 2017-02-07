44fe46d0ce04e2778c6377b1f1ee790ecc17c51d
Katzen48 <Katzen48@freenet.de>
2017-02-08 00:10:11 +0100

    Changed Name of Mainclass

--- src/main/java/com/havensden/utilities/HavensDenUtilities.java
+++ src/main/java/com/havensden/utilities/HavensDenUtilities.java
@@ -5,8 +5,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = HavensDenUtilities.MODID, version = HavensDenUtilities.VERSION)
+public class HavensDenUtilities
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
