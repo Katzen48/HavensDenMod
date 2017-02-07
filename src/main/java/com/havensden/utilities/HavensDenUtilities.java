53d229e11e74e0064a083b6b037b448d83fec17d
Katzen48 <Katzen48@freenet.de>
2017-02-08 00:14:58 +0100

    Changed Name of Mod in Mainclass Changed Version Number

--- src/main/java/com/havensden/utilities/HavensDenUtilities.java
+++ src/main/java/com/havensden/utilities/HavensDenUtilities.java
@@ -8,13 +8,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = HavensDenUtilities.MODID, version = HavensDenUtilities.VERSION)
 public class HavensDenUtilities
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "HavensDen";
+    public static final String VERSION = "0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    	
     }
 }
